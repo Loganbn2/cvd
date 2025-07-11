f05d398ff360b31f53ec6575f78bb5272815f433
Loganbn2 <your-email@example.com>
2025-07-11 04:47:41 -0600

    render template

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def member_portal():
     return render_template('member_portal.html')
 
+@app.route('/reset_password.html')
+def reset_password():
+    return render_template('reset_password.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
